d5ff8d784ade4a96a6febb44f280d7862837eb17
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2024-12-17 20:00:26 +0900

    fixed: ranking error 해결

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,209 +1,214 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Quiz.module.css';
-import lefttop from './lefttop.png';
-import leftbottom from './leftbottom.png';
-import righttop from './righttop.png';
-import point from './point.png';
-import Layout from '../../Layout';
+import React, { useEffect, useState, useCallback, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Quiz.module.css";
+import lefttop from "./lefttop.png";
+import leftbottom from "./leftbottom.png";
+import righttop from "./righttop.png";
+import point from "./point.png";
+import Layout from "../../Layout";
 
 const Quiz = () => {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(true); // 초기 로딩 상태는 true
-    const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true); // 초기 로딩 상태는 true
+  const [error, setError] = useState(null);
 
-    const [notLoading, setNotLoading] = useState(false);
-    const maxBackPress = 3; // 뒤로가기 조롱 메시지 출력 조건
-    const backPressCountRef = useRef(0);
+  const [notLoading, setNotLoading] = useState(false);
+  const maxBackPress = 3; // 뒤로가기 조롱 메시지 출력 조건
+  const backPressCountRef = useRef(0);
 
-    useEffect(() => {
-        const preventGoBack = () => {
-            backPressCountRef.current += 1;
+  useEffect(() => {
+    const preventGoBack = () => {
+      backPressCountRef.current += 1;
 
-            if (backPressCountRef.current >= maxBackPress) {
-                alert('뒤로가기 계엄령! 한번만 더 하면 너 진짜 엄혜영이야!');
-            } else {
-                alert('뒤로가기는 안됩니다!');
-            }
+      if (backPressCountRef.current >= maxBackPress) {
+        alert("뒤로가기 계엄령! 한번만 더 하면 너 진짜 엄혜영이야!");
+      } else {
+        alert("뒤로가기는 안됩니다!");
+      }
 
-            window.history.pushState(null, '', window.location.href);
-        };
+      window.history.pushState(null, "", window.location.href);
+    };
 
-        window.history.pushState(null, '', window.location.href);
-        window.addEventListener('popstate', preventGoBack);
+    window.history.pushState(null, "", window.location.href);
+    window.addEventListener("popstate", preventGoBack);
 
-        return () => {
-            window.removeEventListener('popstate', preventGoBack);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("popstate", preventGoBack);
+    };
+  }, []);
 
-    const [selectedQuizzes, setSelectedQuizzes] = useState(() => {
-        const storedQuizzes = localStorage.getItem('selectedQuizzes');
-        return storedQuizzes ? JSON.parse(storedQuizzes) : [];
-    });
+  const [selectedQuizzes, setSelectedQuizzes] = useState(() => {
+    const storedQuizzes = localStorage.getItem("selectedQuizzes");
+    return storedQuizzes ? JSON.parse(storedQuizzes) : [];
+  });
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
-        () => parseInt(localStorage.getItem('currentQuestionIndex'), 10) || 0
-    );
-    const [correctAnswersCount, setCorrectAnswersCount] = useState(
-        () => parseInt(localStorage.getItem('correctAnswersCount'), 10) || 0
-    );
-    const [score, setScore] = useState(() => parseInt(localStorage.getItem('score'), 10) || 0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
+    () => parseInt(localStorage.getItem("currentQuestionIndex"), 10) || 0
+  );
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(
+    () => parseInt(localStorage.getItem("correctAnswersCount"), 10) || 0
+  );
+  const [score, setScore] = useState(() => parseInt(localStorage.getItem("score"), 10) || 0);
 
-    const randomQuiz = selectedQuizzes.length > 0 ? selectedQuizzes[currentQuestionIndex] : null;
+  const randomQuiz = selectedQuizzes.length > 0 ? selectedQuizzes[currentQuestionIndex] : null;
 
-    useEffect(() => {
-        const fetchQuizData = async () => {
-            try {
-                if (!selectedQuizzes || selectedQuizzes.length === 0) {
-                    const response = await fetch('/example.json');
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const jsonData = await response.json();
+  useEffect(() => {
+    const fetchQuizData = async () => {
+      try {
+        if (!selectedQuizzes || selectedQuizzes.length === 0) {
+          const response = await fetch("/example.json");
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const jsonData = await response.json();
 
-                    const levels = {
-                        'super-difficult': [],
-                        difficult: [],
-                        intermediate: [],
-                        easy: [],
-                    };
+          const levels = {
+            "super-difficult": [],
+            difficult: [],
+            intermediate: [],
+            easy: [],
+          };
 
-                    Object.keys(jsonData).forEach((level) => {
-                        jsonData[level].forEach((item) => {
-                            levels[level].push({ ...item, level });
-                        });
-                    });
+          Object.keys(jsonData).forEach((level) => {
+            jsonData[level].forEach((item) => {
+              levels[level].push({ ...item, level });
+            });
+          });
 
-                    const selectRandomQuestions = (questions, min, max) => {
-                        const count = Math.floor(Math.random() * (max - min + 1)) + min;
-                        return questions.sort(() => Math.random() - 0.5).slice(0, count);
-                    };
+          const selectRandomQuestions = (questions, min, max) => {
+            const count = Math.floor(Math.random() * (max - min + 1)) + min;
+            return questions.sort(() => Math.random() - 0.5).slice(0, count);
+          };
 
-                    let selectedQuestions = [
-                        ...selectRandomQuestions(levels['super-difficult'], 1, 5),
-                        ...selectRandomQuestions(levels['difficult'], 1, 5),
-                        ...selectRandomQuestions(levels['intermediate'], 1, 5),
-                        ...selectRandomQuestions(levels['easy'], 1, 5),
-                    ];
+          let selectedQuestions = [
+            ...selectRandomQuestions(levels["super-difficult"], 1, 5),
+            ...selectRandomQuestions(levels["difficult"], 1, 5),
+            ...selectRandomQuestions(levels["intermediate"], 1, 5),
+            ...selectRandomQuestions(levels["easy"], 1, 5),
+          ];
 
-                    selectedQuestions = selectedQuestions.sort(() => Math.random() - 0.5).slice(0, 10);
+          selectedQuestions = selectedQuestions.sort(() => Math.random() - 0.5).slice(0, 10);
 
-                    selectedQuestions.forEach((quiz) => {
-                        quiz.answer = quiz.answer.sort(() => Math.random() - 0.5);
-                    });
+          selectedQuestions.forEach((quiz) => {
+            quiz.answer = quiz.answer.sort(() => Math.random() - 0.5);
+          });
 
-                    setSelectedQuizzes(selectedQuestions);
-                    localStorage.setItem('selectedQuizzes', JSON.stringify(selectedQuestions));
-                }
-                setLoading(false); // 데이터를 가져온 후 로딩 상태를 false로 변경
-            } catch (err) {
-                setError(err.message);
-                setLoading(false); // 에러가 발생해도 로딩 상태를 false로 변경
-            }
-        };
-
-        fetchQuizData();
-    }, [selectedQuizzes]);
-
-    const handleDivClick = useCallback(
-        (answer, isRight) => {
-            if (!randomQuiz) return;
-
-            const correctAnswer = randomQuiz.answer.find((ans) => ans.TRUE)?.TRUE;
-            const selectedAnswer = answer.TRUE || answer.FALSE;
-
-            const newQuestionIndex = currentQuestionIndex + 1;
-            const newCorrectAnswersCount = isRight ? correctAnswersCount + 1 : correctAnswersCount;
-            const newScore = isRight ? score + 10 : score;
-
-            setCurrentQuestionIndex(newQuestionIndex);
-            setCorrectAnswersCount(newCorrectAnswersCount);
-            setScore(newScore);
-
-            localStorage.setItem('currentQuestionIndex', newQuestionIndex);
-            localStorage.setItem('correctAnswersCount', newCorrectAnswersCount);
-            localStorage.setItem('score', newScore);
-
-            if (newQuestionIndex < 10) {
-                navigate(
-                    `/answer?isRight=${isRight}&correctAnswer=${encodeURIComponent(
-                        correctAnswer
-                    )}&selectedAnswer=${encodeURIComponent(selectedAnswer)}`
-                );
-            } else {
-                localStorage.setItem('score', newScore);
-                localStorage.setItem('totalQuestions', 10);
-
-                localStorage.removeItem('selectedQuizzes');
-                localStorage.removeItem('currentQuestionIndex');
-                localStorage.removeItem('correctAnswersCount');
-                setNotLoading(true);
-            }
-        },
-        [randomQuiz, currentQuestionIndex, correctAnswersCount, score, navigate]
-    );
-
-    useEffect(() => {
-        if (notLoading) {
-            navigate(`/result`);
+          setSelectedQuizzes(selectedQuestions);
+          localStorage.setItem("selectedQuizzes", JSON.stringify(selectedQuestions));
         }
-    }, [notLoading, navigate]);
+        setLoading(false); // 데이터를 가져온 후 로딩 상태를 false로 변경
+      } catch (err) {
+        setError(err.message);
+        setLoading(false); // 에러가 발생해도 로딩 상태를 false로 변경
+      }
+    };
 
-    // 로딩 상태 처리
-    if (loading) {
-        return <div>Loading...</div>;
+    fetchQuizData();
+  }, [selectedQuizzes]);
+
+  //의존성 배열에 있는 값이 변경될 때만 함수가 재생성되므로, 성능 최적화에 유용합니다.
+  const handleDivClick = useCallback(
+    (answer, isRight) => {
+      if (!randomQuiz) return;
+
+      const correctAnswer = randomQuiz.answer.find((ans) => ans.TRUE)?.TRUE;
+      const selectedAnswer = answer.TRUE || answer.FALSE;
+
+      const newQuestionIndex = currentQuestionIndex + 1;
+      const newCorrectAnswersCount = isRight ? correctAnswersCount + 1 : correctAnswersCount;
+      const newScore = isRight ? score + 10 : score;
+
+      setCurrentQuestionIndex(newQuestionIndex);
+      setCorrectAnswersCount(newCorrectAnswersCount);
+      setScore(newScore);
+
+      localStorage.setItem("currentQuestionIndex", newQuestionIndex);
+      localStorage.setItem("correctAnswersCount", newCorrectAnswersCount);
+      localStorage.setItem("score", newScore);
+
+      if (newQuestionIndex < 10) {
+        // 마지막 문제가 아니면 기존 로직 그대로
+        navigate(
+          `/answer?isRight=${isRight}&correctAnswer=${encodeURIComponent(
+            correctAnswer
+          )}&selectedAnswer=${encodeURIComponent(selectedAnswer)}`
+        );
+      } else {
+        // 마지막 문제를 푼 경우
+        localStorage.setItem("score", newScore);
+        localStorage.setItem("totalQuestions", 10);
+
+        // 기존에 여기서 제거하던 localStorage 항목 제거 로직 삭제
+        // localStorage.removeItem('selectedQuizzes');
+        // localStorage.removeItem('currentQuestionIndex');
+        // localStorage.removeItem('correctAnswersCount');
+
+        setNotLoading(true);
+      }
+    },
+    [randomQuiz, currentQuestionIndex, correctAnswersCount, score, navigate]
+  );
+
+  useEffect(() => {
+    if (notLoading) {
+      navigate(`/result`);
     }
+  }, [notLoading, navigate]);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+  // 로딩 상태 처리
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    if (!randomQuiz) {
-        return <div>No quiz data available</div>; // 퀴즈 데이터가 없을 때 처리
-    }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
-    const circleClasses = [styles.circle, styles.circle2, styles.circle3];
+  if (!randomQuiz) {
+    return <div>No quiz data available</div>; // 퀴즈 데이터가 없을 때 처리
+  }
 
-    return (
-        <Layout>
-            <div className={styles.container}>
-                <div className={styles.topdiv}>
-                    <img className={styles.lefttop} src={lefttop} alt="Left Top" />
-                    <img className={styles.righttop} src={righttop} alt="Right Top" />
-                    <div className={styles.pointShow}>
-                        <div>
-                            <img className={styles.point} src={point} alt="point" />
-                        </div>
-                        <div className={styles.pointText}>점수: {score}</div>
-                    </div>
-                </div>
-                <div className={styles.quizShow}>
-                    <div className={styles.quizText}>{randomQuiz?.quiz || '퀴즈 데이터를 불러오는 중...'}</div>
-                </div>
-                <div className={styles.answerShow}>
-                    {randomQuiz.answer.map((ans, index) => (
-                        <div
-                            key={index}
-                            className={`${styles.answerDiv} ${index === 1 ? styles.midAnswerDiv : ''}`}
-                            onClick={() => handleDivClick(ans, ans.TRUE !== undefined)}
-                        >
-                            <div className={circleClasses[index % circleClasses.length]}>
-                                <div className={styles.circleText}>{index + 1}</div>
-                            </div>
-                            <div className={styles.rectangle}>
-                                <div className={styles.rectangleText}>{ans.TRUE ? ans.TRUE : ans.FALSE}</div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div className={styles.numShow}>{currentQuestionIndex + 1} / 10</div>
-                <div className={styles.bottomdiv}>
-                    <img className={styles.leftbottom} src={leftbottom} alt="Left Bottom" />
-                </div>
+  const circleClasses = [styles.circle, styles.circle2, styles.circle3];
+
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.topdiv}>
+          <img className={styles.lefttop} src={lefttop} alt="Left Top" />
+          <img className={styles.righttop} src={righttop} alt="Right Top" />
+          <div className={styles.pointShow}>
+            <div>
+              <img className={styles.point} src={point} alt="point" />
             </div>
-        </Layout>
-    );
+            <div className={styles.pointText}>점수: {score}</div>
+          </div>
+        </div>
+        <div className={styles.quizShow}>
+          <div className={styles.quizText}>{randomQuiz?.quiz || "퀴즈 데이터를 불러오는 중..."}</div>
+        </div>
+        <div className={styles.answerShow}>
+          {randomQuiz.answer.map((ans, index) => (
+            <div
+              key={index}
+              className={`${styles.answerDiv} ${index === 1 ? styles.midAnswerDiv : ""}`}
+              onClick={() => handleDivClick(ans, ans.TRUE !== undefined)}
+            >
+              <div className={circleClasses[index % circleClasses.length]}>
+                <div className={styles.circleText}>{index + 1}</div>
+              </div>
+              <div className={styles.rectangle}>
+                <div className={styles.rectangleText}>{ans.TRUE ? ans.TRUE : ans.FALSE}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className={styles.numShow}>{currentQuestionIndex + 1} / 10</div>
+        <div className={styles.bottomdiv}>
+          <img className={styles.leftbottom} src={leftbottom} alt="Left Bottom" />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Quiz;
--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,143 +1,141 @@
-import React, { useEffect, useState } from 'react';
-import html2canvas from 'html2canvas';
-import styles from './Ranking.module.css';
-import Layout from '../../Layout';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import html2canvas from "html2canvas";
+import styles from "./Ranking.module.css";
+import Layout from "../../Layout";
+import { useNavigate } from "react-router-dom";
 
 const Ranking = () => {
-    const navigate = useNavigate();
-    const [rankings, setRankings] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [userPosition, setUserPosition] = useState(null);
-    const [nickname, setNickname] = useState('');
-    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  const navigate = useNavigate();
+  const [rankings, setRankings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [userPosition, setUserPosition] = useState(null);
+  const [nickname, setNickname] = useState("");
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-    const handleRetry = () => {
-        localStorage.removeItem('currentQuestionIndex');
-        localStorage.removeItem('correctAnswersCount');
-        localStorage.removeItem('score');
-        localStorage.removeItem('totalQuestions');
-        localStorage.removeItem('selectedQuizzes');
-        navigate('/');
-    };
+  const handleRetry = () => {
+    localStorage.removeItem("currentQuestionIndex");
+    localStorage.removeItem("correctAnswersCount");
+    localStorage.removeItem("score");
+    localStorage.removeItem("totalQuestions");
+    localStorage.removeItem("selectedQuizzes");
+    navigate("/");
+  };
 
-    const handleShare = async () => {
-        try {
-            // 전체 페이지 스크린샷 (축소)
-            const canvas = await html2canvas(document.body, { scale: 0.5 });
-            const imageData = canvas.toDataURL('image/png');
+  const handleShare = async () => {
+    try {
+      // 전체 페이지 스크린샷 (축소)
+      const canvas = await html2canvas(document.body, { scale: 0.5 });
+      const imageData = canvas.toDataURL("image/png");
 
-            // 로컬에 이미지 저장
-            const link = document.createElement('a');
-            link.href = imageData;
-            link.download = 'ranking-result.png';
-            link.click();
+      // 로컬에 이미지 저장
+      const link = document.createElement("a");
+      link.href = imageData;
+      link.download = "ranking-result.png";
+      link.click();
 
-            // Web Share API 시도
-            if (navigator.share) {
-                const response = await fetch(imageData);
-                const blob = await response.blob();
-                const file = new File([blob], 'ranking-result.png', { type: 'image/png' });
+      // Web Share API 시도
+      if (navigator.share) {
+        const response = await fetch(imageData);
+        const blob = await response.blob();
+        const file = new File([blob], "ranking-result.png", { type: "image/png" });
 
-                await navigator.share({
-                    title: '랭킹 결과 공유',
-                    text: '내 랭킹 결과를 확인해봐!',
-                    files: [file],
-                });
-            } else {
-                alert(
-                    '이 브라우저는 Web Share API를 지원하지 않습니다.\n이미지를 저장했습니다. ' +
-                        '인스타그램 앱을 열어 스토리에 수동으로 업로드해주세요!'
-                );
-            }
-        } catch (error) {
-            console.error('스크린샷 저장 및 공유 중 오류:', error);
-            alert('결과 저장에 실패했습니다. 스크린샷이 저장되었는지 확인 후 수동으로 업로드해주세요.');
+        await navigator.share({
+          title: "랭킹 결과 공유",
+          text: "내 랭킹 결과를 확인해봐!",
+          files: [file],
+        });
+      } else {
+        alert(
+          "이 브라우저는 Web Share API를 지원하지 않습니다.\n이미지를 저장했습니다. " +
+            "인스타그램 앱을 열어 스토리에 수동으로 업로드해주세요!"
+        );
+      }
+    } catch (error) {
+      console.error("스크린샷 저장 및 공유 중 오류:", error);
+      alert("결과 저장에 실패했습니다. 스크린샷이 저장되었는지 확인 후 수동으로 업로드해주세요.");
+    }
+  };
+
+  useEffect(() => {
+    const fetchRankings = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/api/ranking`);
+        if (!response.ok) throw new Error("Network response was not ok");
+
+        const jsonData = await response.json();
+        const userNickname = localStorage.getItem("nickname");
+        const score = parseInt(localStorage.getItem("score"), 10);
+
+        let highScore = parseInt(localStorage.getItem("highScore"), 10) || 0;
+        setNickname(userNickname);
+
+        let updatedRankings = jsonData.rankings;
+
+        if (userNickname && !isNaN(score)) {
+          if (score > highScore) {
+            highScore = score;
+            localStorage.setItem("highScore", highScore);
+          }
+
+          updatedRankings = [...updatedRankings, { nickname: userNickname, score: highScore }];
+          updatedRankings.sort((a, b) => b.score - a.score);
+
+          updatedRankings.forEach((rank, index) => {
+            rank.position = index + 1;
+          });
+
+          const userRank = updatedRankings.find((rank) => rank.nickname === userNickname && rank.score === highScore);
+          setUserPosition(userRank ? userRank.position : null);
         }
+
+        setRankings(updatedRankings);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
 
-    useEffect(() => {
-        const fetchRankings = async () => {
-            try {
-                const response = await fetch(`${BACKEND_URL}/api/ranking`);
-                if (!response.ok) throw new Error('Network response was not ok');
+    fetchRankings();
+  }, [BACKEND_URL]);
 
-                const jsonData = await response.json();
-                const userNickname = localStorage.getItem('nickname');
-                const score = parseInt(localStorage.getItem('score'), 10);
+  if (loading) {
+    return <div className={styles.loading}>Loading...</div>;
+  }
 
-                let highScore = parseInt(localStorage.getItem('highScore'), 10) || 0;
-                setNickname(userNickname);
+  if (error) {
+    return <div className={styles.error}>Error: {error}</div>;
+  }
 
-                let updatedRankings = jsonData.rankings;
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          {nickname ? `너 자신을 알라 ${nickname}, 너는 ${userPosition}등` : "너 자신을 알라"}
+        </div>
+        <div className={styles.rankBox}>
+          <div className={styles.rankTitle}>RANK</div>
+          <div className={styles.rankList}>
+            {rankings.map((rank, index) => (
+              <div key={`${rank.nickname}-${index}`} className={styles.rankItem}>
+                {rank.position}. {rank.nickname} - {rank.score}점
+              </div>
+            ))}
+          </div>
+        </div>
 
-                if (userNickname && !isNaN(score)) {
-                    if (score > highScore) {
-                        highScore = score;
-                        localStorage.setItem('highScore', highScore);
-                    }
-
-                    updatedRankings = [...updatedRankings, { nickname: userNickname, score: highScore }];
-                    updatedRankings.sort((a, b) => b.score - a.score);
-
-                    updatedRankings.forEach((rank, index) => {
-                        rank.position = index + 1;
-                    });
-
-                    const userRank = updatedRankings.find(
-                        (rank) => rank.nickname === userNickname && rank.score === highScore
-                    );
-                    setUserPosition(userRank ? userRank.position : null);
-                }
-
-                setRankings(updatedRankings);
-                setLoading(false);
-            } catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
-
-        fetchRankings();
-    }, [BACKEND_URL]);
-
-    if (loading) {
-        return <div className={styles.loading}>Loading...</div>;
-    }
-
-    if (error) {
-        return <div className={styles.error}>Error: {error}</div>;
-    }
-
-    return (
-        <Layout>
-            <div className={styles.container}>
-                <div className={styles.title}>
-                    {nickname ? `너 자신을 알라 ${nickname}, 너는 ${userPosition}등` : '너 자신을 알라'}
-                </div>
-                <div className={styles.rankBox}>
-                    <div className={styles.rankTitle}>RANK</div>
-                    <div className={styles.rankList}>
-                        {rankings.map((rank, index) => (
-                            <div key={`${rank.nickname}-${index}`} className={styles.rankItem}>
-                                {rank.position}. {rank.nickname} - {rank.score}점
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className={styles.shareButtons}>
-                    <button className={styles.retryButton} onClick={handleRetry}>
-                        RETRY?
-                    </button>
-                    <button className={styles.instagramButton} onClick={handleShare}>
-                        <span className={styles.instagramIcon}></span> 결과 저장 & 공유하기
-                    </button>
-                </div>
-            </div>
-        </Layout>
-    );
+        <div className={styles.shareButtons}>
+          <button className={styles.retryButton} onClick={handleRetry}>
+            RETRY?
+          </button>
+          <button className={styles.instagramButton} onClick={handleShare}>
+            <span className={styles.instagramIcon}></span> 결과 저장 & 공유하기
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Ranking;
