f0e47dcb4ba06fa9e5159902432064c4dc66922b
이준형 <gaejungrhee@ijunhyeong-ui-MacBookPro.local>
2024-12-27 20:18:16 +0900

    ranking.js 도발 멘트 수정

--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -128,7 +128,7 @@ const Ranking = () => {
                         <div className={styles.provocationBox}>
                             <h3>결과 공유 안 하면...</h3>
                             <p>
-                                이대로 포기해버리는 건가요?
+                                이대로 석열이처럼 포기해버리는 건가요?
                                 <br />
                                 <b>“아니 뭐... {nickname || '익명'} 정도면 자기 점수 공개하기 쪽팔리다는 건가?”</b>
                                 <br />
